1fec409e2f53eb5fb0435d241c45c6e84acbcfbb
SirAlex36 <alexngenoo@gmail.com>
2026-09-21 14:34:48 +0300

    fixed tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#3B82F6',
-        secondary: '#1F2937',
+        primary: "#3B82F6",
+        secondary: "#1F2937",
+
+        border: "var(--border)",
+        input: "var(--input)",
+
+        ring: {
+          DEFAULT: "var(--ring)",
+        },
+
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        card: "var(--card)",
+        "card-foreground": "var(--card-foreground)",
+        popover: "var(--popover)",
+        "popover-foreground": "var(--popover-foreground)",
+        muted: "var(--muted)",
+        "muted-foreground": "var(--muted-foreground)",
+        accent: "var(--accent)",
+        "accent-foreground": "var(--accent-foreground)",
+        destructive: "var(--destructive)",
+        "destructive-foreground": "var(--destructive-foreground)",
       },
     },
   },
   plugins: [],
-}
+};
